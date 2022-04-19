f7bb1bce573af6623d006c5a5dde08c271bebf88
Ecantero <38026174+Ecantero@users.noreply.github.com>
2022-04-19 08:10:59 -0600

    fixed code syntax error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Navigation } from './Components/Navigation';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
