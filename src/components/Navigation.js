b14dea367ce856d9767f52a54c4e2ec4915f31d6
mirandashaekelley <mkelley@student.neumont.edu>
2022-04-21 08:30:16 -0600

    navbar with logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Navigation.css"
 import { Link } from 'react-router-dom';
-
+import logo from '../assets/popify-logo.png';
 
 export const Navigation = () => {
   return (
     <div class="container">
-      <div class="logo">popify</div>
+      <div class="logo">
+        <img src={logo} alt="popify logo"/>
+      </div>
       <div class="links">
         <div>
           <Link to="/Login">Login</Link>
