a3ab4f04e08a16951aedb1c7b329ee9b874c0426
Joshua <jgorham@student.neumont.edu>
2022-05-10 09:15:05 -0600

    Music is added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,11 +5,11 @@ import logo from '../assets/popify-logo.png';
 
 export const Navigation = () => {
   return (
-    <div class="container">
-      <div class="logo">
+    <div className="container">
+      <div className="logo">
         <img src={logo} alt="popify logo"/>
       </div>
-      <div class="links">
+      <div className="links">
         <div>
           <Link to="/Login">Login</Link>
         </div>
