c669b09138243597392115ebee25a63d6a23ecde
Lemuel Bonite <ljbonite21@gmail.com>
2022-05-26 08:48:12 -0600

    added aws-sdk

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import * as AWS from 'aws-sdk';
+import { ConfigurationOptions } from 'aws-sdk';
+
+ConfigurationOptions = {
+    region: 'us-west-1',
+    secretAccessKey: '1ANCxy56WfjmLFhFjveFhbcK7swZk2RS9c8olfw5',
+    accessKeyId: 'AKIAUBQP5LDCTKAXPKOQ'
+}
+
+AWS.config.update(configuration)
+
 ReactDOM.render(
   <React.StrictMode>
       <App/>
