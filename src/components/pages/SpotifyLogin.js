ef926e571ac94014df8aa46ac28e19d01d86399e
Ecantero <38026174+Ecantero@users.noreply.github.com>
2022-05-27 09:00:57 -0600

    checking to see if thes spotify code works

--- src/components/pages/SpotifyLogin.js
+++ src/components/pages/SpotifyLogin.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Signup.css";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=ed8c5f5cbf984d1791368e0cd7bf97a5&response_type=code&redirect_uri=http://localhost:3000/home&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=ed8c5f5cbf984d1791368e0cd7bf97a5&response_type=code&redirect_uri=https://aws-host.d3021ujnmxl8cr.amplifyapp.com/home&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 const SpotifyLogin = () => {
   return (
     <div className="formContainer">
-      <a className="button" href={AUTH_URL}>Login with Spotify</a>
+      <a className="button" href={AUTH_URL}>
+        Login with Spotify
+      </a>
     </div>
   );
 };
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function useAuth(code) {
+const useAuth = (code) => {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
@@ -14,6 +14,7 @@ export default function useAuth(code) {
         code,
       })
       .then((res) => {
+        console.log(res);
         setAccessToken(res.data.accessToken);
         setRefreshToken(res.data.refreshToken);
         setExpiresIn(res.data.expiresIn);
@@ -28,7 +29,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return;
     const interval = setInterval(() => {
       axios
-        .post("http://localhost:3001/refresh", {
+        .post("https://1cdivxrxef.execute-api.us-west-1.amazonaws.com/prod/refresh", {
           refreshToken,
         })
         .then((res) => {
@@ -45,3 +46,5 @@ export default function useAuth(code) {
 
   return accessToken;
 }
+
+export default useAuth;
