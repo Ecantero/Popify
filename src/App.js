9841d7c43265505f565e88d1ad2e839767bc7688
mirandashaekelley <mkelley@student.neumont.edu>
2022-05-30 18:36:47 -0600

    make sure to run npm i

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { Routing } from './routes/routes';
 import React from 'react';
 import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 const App=()=> {
 
